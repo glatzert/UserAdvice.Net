4f25a21574b42d01dffc8eac55e01e3d554f5f20
Thomas Glatzer <glatzert@outlook.de>
2018-12-31 13:36:23 +0100

    IndexPage ViewModels

---
+++ src/UserAdvice.App/ViewModel/Category.cs
@@ -0,0 +1,25 @@
+﻿namespace UserAdvice.ViewModel
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string UrlSegment { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public int SortKey { get; set; }
+    }
+
+    public class CategoryRef
+    {
+        public int Id { get; set; }
+
+        public string UrlSegment { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/UserAdvice.App/ViewModel/PostTeaser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserAdvice.ViewModel
+{
+    public class PostTeaser
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string VoteCount { get; set; }
+        public string CommentCount { get; set; }
+
+        public bool UserVoted { get; set; }
+
+        public CategoryRef Category { get; set; }
+
+        public List<StatusComment> StatusComments { get; set; }
+
+        public class StatusComment
+        {
+            public string Content { get; set; }
+
+            public string StatusName { get; set; }
+            public string ForegroundColor { get; set; }
+
+            public string CreatedBy { get; set; }
+            public DateTimeOffset CreatedAt { get; set; }
+        }
+    }
+}
