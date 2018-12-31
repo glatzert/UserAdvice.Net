7b638b6eb15eeb0f60c950cb3d973f50d29a8519
Thomas Glatzer <glatzert@outlook.de>
2018-12-31 13:37:24 +0100

    Data Mapping

---
+++ src/UserAdvice.App/Data/Mapping/ViewModelMapper.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+
+namespace UserAdvice.Data.Mapping
+{
+    internal static class ViewModelMapper
+    {
+        private static IMapper _mapper;
+
+        static ViewModelMapper()
+        {
+            _mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MapperProfile>();
+            }).CreateMapper();
+        }
+
+        public static ViewModel.PostTeaser ToViewModel(this Entities.Post post)
+        {
+            var result = _mapper.Map<ViewModel.PostTeaser>(post);
+
+            return result;
+        }
+
+        internal class MapperProfile : Profile
+        {
+            public MapperProfile()
+            {
+                CreateMap<Entities.Post, ViewModel.PostTeaser>();
+                CreateMap<Entities.Comment, ViewModel.PostTeaser.StatusComment>();
+                CreateMap<Entities.Category, ViewModel.Category>();
+                CreateMap<Entities.Category, ViewModel.CategoryRef>();
+            }
+        }
+    }
+}
