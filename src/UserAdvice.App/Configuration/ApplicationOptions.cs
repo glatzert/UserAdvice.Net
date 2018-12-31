f938727740338956398041dd8079c90f8479819d
Thomas Glatzer <glatzert@outlook.de>
2018-12-31 13:08:36 +0100

    IndexPage Options

---
+++ src/UserAdvice.App/Configuration/ApplicationOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAdvice.Configuration
+{
+    public class ApplicationOptions
+    {
+        public IndexOptions IndexOptions { get; set; }
+    }
+
+    public class IndexOptions
+    {
+        public bool HidePosts { get; set; }
+        public bool IncludeAllCategories { get; set; }
+    }
+}
