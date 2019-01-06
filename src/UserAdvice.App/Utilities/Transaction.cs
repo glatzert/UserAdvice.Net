bce028c75d66c44324b7b86473eb2659449c0285
Thomas Glatzer <glatzert@outlook.de>
2019-01-06 15:58:26 +0100

    Transaction Utility

---
+++ src/UserAdvice.App/Utilities/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Transactions;
+
+namespace UserAdvice.Utilities
+{
+    internal static class Transaction
+    {
+        public static TransactionScope BeginTransaction()
+        {
+            return new TransactionScope(TransactionScopeOption.Required, 
+                new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead },
+                TransactionScopeAsyncFlowOption.Enabled);
+        }
+    }
+}
