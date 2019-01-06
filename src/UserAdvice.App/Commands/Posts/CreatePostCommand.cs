f887256ba07d815631ffa77db126235c74e7f945
Thomas Glatzer <glatzert@outlook.de>
2019-01-06 21:15:49 +0100

    Add CommandStubs

--- src/UserAdvice.App/Commands/Posts/CreatePostCommand.cs
+++ src/UserAdvice.App/Commands/Posts/CreatePostCommand.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Security.Claims;
 
 namespace UserAdvice.Commands.Posts
 {
     public class CreatePostCommand
     {
+        public CreatePostCommand(ClaimsPrincipal currentUser)
+        {
+            CurrentUser = currentUser ?? throw new System.ArgumentNullException(nameof(currentUser));
+        }
 
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public ClaimsPrincipal CurrentUser { get; set; }
     }
-
-    public class AddTagCommand
-    {
-
-    }
-
-    public class RemoveTagCommand
-    {
-
-    }
-
-    public class EditPostCommand
-    {
-
-    }
-
-
 }
---
+++ src/UserAdvice.App/Commands/Posts/RemoveTagCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace UserAdvice.Commands.Posts
+{
+    public class RemoveTagCommand
+    {
+        public RemoveTagCommand(int postId, int tagId,
+            ClaimsPrincipal currentUser)
+        {
+            if (postId <= 0)
+                throw new ArgumentOutOfRangeException(nameof(postId));
+            if (tagId <= 0)
+                throw new ArgumentOutOfRangeException(nameof(tagId));
+
+            PostId = postId;
+            TagId = tagId;
+
+            CurrentUser = currentUser ?? throw new System.ArgumentNullException(nameof(currentUser));
+        }
+
+        public int PostId { get; }
+        public int TagId { get; }
+        public ClaimsPrincipal CurrentUser { get; }
+    }
+}
