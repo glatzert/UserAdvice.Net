0bc9063b13b4fd18646902b70093133857859a9b
Thomas Glatzer <glatzert@outlook.de>
2019-01-06 21:27:17 +0100

    More Commands

---
+++ src/UserAdvice.App/Commands/Posts/MovePostCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace UserAdvice.Commands.Posts
+{
+    public class MovePostCommand
+    {
+        public MovePostCommand(int postId, int? categoryId,
+            ClaimsPrincipal currentUser)
+        {
+            if (postId <= 0)
+                throw new ArgumentOutOfRangeException(nameof(postId));
+            if (categoryId.HasValue && categoryId <= 0)
+                throw new ArgumentOutOfRangeException(nameof(categoryId));
+
+            PostId = postId;
+            CategoryId = categoryId;
+            CurrentUser = currentUser ?? throw new ArgumentNullException(nameof(currentUser));
+        }
+
+        public int PostId { get; }
+        public int? CategoryId { get; }
+
+        public ClaimsPrincipal CurrentUser { get; }
+    }
+}
